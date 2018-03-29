207f0c37243ff3a3fd91f86e81b586839a6d8dc5
Sandro Dzneladze <sandrodz@gmail.com>
2018-03-29 10:18:51 +0400

    feat: Page builder template

---
+++ page-builder.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Name: Page Builder
+ *
+ * Intellectual Property rights, and copyright, reserved by Kubik Ltd. as allowed by law include,
+ * but are not limited to, the working concept, function, and behavior of this software,
+ * the logical code structure and expression as written.
+ *
+ * @package     PugPress Starter Theme
+ * @author      Kubik Ltd. http://kubikplugins.com/
+ * @copyright   Copyright (c) Kubik Ltd. (support@kubikplugins.com)
+ * @since       0.0.1
+ * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ */
+
+$base = include 'library/data/base.php';
+
+$data = array_merge( $base, [
+	'sections' => [
+		[ 'n' => 's-hr' ],
+		[
+			'n' => 's-link',
+			'o' => [
+				'title' => 'Lorem ipsum',
+				'link' => '#',
+			],
+		],
+		[ 'n' => 's-hr' ],
+	],
+] );
+
+if ( function_exists( 'pugpress_render' ) ) {
+	pugpress_render( 'page-builder', $data );
+} else {
+	esc_html_e( 'This theme requires the PugPress plugin in order to work. Please, install it now!', 'pugpress-starter-theme' );
+}
